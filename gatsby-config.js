90ea2270d47c3eb32711bfd1603138935d1b9391
JWWon <wonjiwn@naver.com>
2020-03-06 18:39:07 +0900

    Create project with typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tastie`,
+    description: `내가 뭘 먹고싶은지 나도 모르니까, Tastie`,
+    author: `Tastie Guys`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/utils/createPages.ts
@@ -0,0 +1,5 @@
+import { CreatePagesArgs } from 'gatsby';
+
+export async function createPages({ actions }: CreatePagesArgs) {
+	console.log('I will create a page!');
+}
